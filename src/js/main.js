05db6914425d30306fac9b47be7ebad4d26346e0
Silvestre Vivo <silvestrevivomillan@gmail.com>
2017-03-04 11:08:05 +0100

    Version 1.1

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,3 @@
-// $(document).ready(function(){
-//   $('.banner, h2').addClass('visible');
-//   $('.title').find('li').first().fadeTo('slow', 1 ,  function showNext() {
-//     $(this).next('li').fadeTo('slow', 1,  showNext );
-//   });
-// });
-
-
 $(document).ready(function(){
   $('.banner').fadeIn(1500, function(){
     $('.title-banner').find('li').first().fadeTo('fast', 1 ,  function showNext() {
