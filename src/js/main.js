02b210499b6a1e7ff3c4d11ffd6a6e880aca99bd
Silvestre Vivo <silvestrevivomillan@gmail.com>
2017-03-04 10:51:58 +0100

    Version 1.0

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,15 @@
-function say_Hi(){
-  console.log('Gulp is working OK!!!');
-}
-say_Hi();
+// $(document).ready(function(){
+//   $('.banner, h2').addClass('visible');
+//   $('.title').find('li').first().fadeTo('slow', 1 ,  function showNext() {
+//     $(this).next('li').fadeTo('slow', 1,  showNext );
+//   });
+// });
+
+
+$(document).ready(function(){
+  $('.banner').fadeIn(1500, function(){
+    $('.title-banner').find('li').first().fadeTo('fast', 1 ,  function showNext() {
+    $(this).next('li').fadeTo('fast', 1,  showNext );
+    });
+  });
+});
