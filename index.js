3ca238f57a3c7a43e884863b7a220dce23366bdd
silvestrevivo <silvestrevivomillan@gmail.com>
2018-03-22 09:46:16 +0100

    video in de background

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 import './assets/sass/style.scss';
 import $ from 'jquery';
-console.log('parcel')
-
 
 $(document).ready(function () {
-  $('.banner').fadeIn(1500, function () {
-    $('.title-banner').find('li').first().fadeTo('fast', 1, function showNext() {
-      $(this).next('li').fadeTo('fast', 1, showNext);
-    });
+  $('.title-banner').find('li').first().fadeTo('fast', 1, function showNext() {
+    $(this).next('li').fadeTo('fast', 1, showNext);
   });
 });
